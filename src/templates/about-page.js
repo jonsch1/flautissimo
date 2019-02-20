de86c318dab8bf7f78075658dd483125348fb928
jonsch1 <j.schaul@student.maastrichtuniversity.nl>
2019-02-20 11:58:47 +0100

    Neu

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -8,8 +8,29 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
-    <section className="section section--gradient">
+    <section className="section section--gradient margin-bottom">
+
       <div className="container">
+        <div className="content">
+          <div
+            className="full-width-image-container margin-top-0"
+            style={{
+              backgroundImage: `url('/img/Musikladen-Aachen-Frontansicht.jpg')`,
+            }}
+          >
+            <h1
+              className="has-text-weight-bold is-size-1"
+              style={{
+                boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
+                backgroundColor: '#f40',
+                color: 'white',
+                padding: '1rem',
+              }}
+            >
+              Über uns
+            </h1>
+          </div>
+          </div>
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <div className="section">
@@ -36,11 +57,12 @@ const AboutPage = ({ data }) => {
 
   return (
     <Layout>
-      <AboutPageTemplate
-        contentComponent={HTMLContent}
-        title={post.frontmatter.title}
-        content={post.html}
-      />
+        <AboutPageTemplate
+          contentComponent={HTMLContent}
+          title={post.frontmatter.title}
+          content={post.html}
+        />
+
     </Layout>
   )
 }
